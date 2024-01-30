578ab94ef73c731e048b77e28bdbb35b2f9cf274
cs-488-01 <cs-488-01@maclabcs17.lclark.edu>
2024-01-30 15:30:35 -0800

    Jan30Code2

--- main.py
+++ main.py
@@ -1,52 +1,111 @@
 # Example file showing a basic pygame "game loop"
 import pygame
 
-# pygame setup
-pygame.init()
-screen = pygame.display.set_mode((1280, 720))
-clock = pygame.time.Clock()
-running = True
 
-while running:
-    # poll for events
-    # pygame.QUIT event means the user clicked X to close your window
-    for event in pygame.event.get():
-        if event.type == pygame.QUIT:
-            running = False
+def get_num_players():
+    num_players = 0
+    while num_players < 2 or num_players > 6 :
+        num_players = int(input("How many players?: "))
 
+    return num_players
+
+
+def screen_update(screen, players):
     # fill the screen with a color to wipe away anything from last frame
     screen.fill("white")
+    board1_location = (50, 100)
+    board2_location = (50, 470)
+    create_board(board1_location, screen)
+    create_board(board2_location, screen)
 
-    x =50
-    y =50
-    w =50
-    h =50
+    if players >= 3:
+        board3_location = (830, 100)
+        create_board(board3_location, screen)
+
+    if players >= 4:
+        board4_location = (830, 470)
+        create_board(board4_location, screen)
+
+    if players >= 5:
+        board5_location = (50, 840)
+        create_board(board5_location, screen)
+
+    if players >= 6:
+        board5_location = (830, 840)
+        create_board(board5_location, screen)
+
+    # flip() the display to put your work on screen
+    pygame.display.flip()
 
 
-    colors = [pygame.Color(250,219,216,216), pygame.Color(252,243,207,207), pygame.Color(213,245,227,227), pygame.Color(214,234,248,248)]
+def main():
+    #game setup
+    players = get_num_players()
+    pygame.init()
+    clock = pygame.time.Clock()
+    running = True
+
+    screen = pygame.display.set_mode((810, 850))
+
+    if players == 3 or players == 4:
+        screen = pygame.display.set_mode((1610, 850))
+
+    if players == 5 or players == 6:
+        screen = pygame.display.set_mode((1610, 1220))
+
+        screen_update(screen, players)
+
+
+    while running:
+        # poll for events
+        # pygame.QUIT event means the user clicked X to close your window
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                running = False
+
+            if event.type == pygame.MOUSEBUTTONDOWN:
+                position = pygame.mouse.get_pos()
+                print("mouse clicked at", position)
+
+                screen_update(screen, players)
+
+
+
+        clock.tick(60)  # limits FPS to 60
+
+    pygame.quit()
+
+def create_board(location, screen):
+    x, y = location
+    w = 50
+    h = 50
+
+    colors = [pygame.Color(250, 219, 216, 216), pygame.Color(252, 243, 207, 207), pygame.Color(213, 245, 227, 227),
+              pygame.Color(214, 234, 248, 248)]
     font = pygame.font.Font('freesansbold.ttf', 32)
     signs_font = pygame.font.Font('freesansbold.ttf', 22)
     penalty_font = pygame.font.Font('freesansbold.ttf', 20)
     c = 0
-    border = pygame.Rect(x-10, y-10, (x+10)*12+10, (y+10)*6+10)
+
+    #create background rectangle - 730 by 320 pixels
+    border = pygame.Rect(x - 10, y - 10, 730, 320)
     pygame.draw.rect(screen, "gray", border)
     workingx = x
 
     penalty = penalty_font.render("penalty", True, "black", None)
     penaltyRect = penalty.get_rect()
-    penaltyRect.center = (workingx+60, y * 6)
+    penaltyRect.center = (workingx + 60, y + 250)
     screen.blit(penalty, penaltyRect)
     for i in range(4):
-        box = pygame.Rect(workingx, y*6+15, 20, 20)
+        box = pygame.Rect(workingx, y + 265, 20, 20)
         pygame.draw.rect(screen, "white", box)
-        workingx+=30
+        workingx += 30
 
-
-    workingx+=20
+    workingx += 20
     for i in range(6):
-        score = pygame.Rect(workingx, y*6, 75, 40)
+        score = pygame.Rect(workingx, y + 250, 75, 40)
         pygame.draw.rect(screen, "white", score)
-        workingx+=95
+        workingx += 95
         if i < 2:
             signs = signs_font.render("+", True, "black", None)
         if i == 3:
@@ -56,37 +115,35 @@ while running:
         if i == 5:
             signs = signs_font.render(" ", True, "black", None)
         signsRect = penalty.get_rect()
-        signsRect.center = (workingx +23, y*6 +20)
+        signsRect.center = (workingx + 23, y + 250 + 20)
         screen.blit(signs, signsRect)
 
+    workingy=y
 
     for color in colors:
-        c+= 1
-
+        c += 1
+        workingx=x
         for num in range(12):
-            board = pygame.Rect(x, y, w, h)
+
+            board = pygame.Rect(workingx, workingy, w, h)
             pygame.draw.rect(screen, color, board)
-            x += 60
+            workingx += 60
             if c > 2:
-                disp_num = (num-12)*-1
+                disp_num = (num - 12) * -1
 
             else:
-                disp_num = num+2
+                disp_num = num + 2
 
-            if disp_num == 13 or disp_num ==1:
+            if disp_num == 13 or disp_num == 1:
                 numbers = font.render("L", True, "grey", None)
             else:
                 numbers = font.render(str(disp_num), True, "grey", None)
 
             numbersRect = numbers.get_rect()
-            numbersRect.center = (x-(w/2)-10, y+(h/2) )
+            numbersRect.center = (workingx - (w / 2) - 10, workingy + (h / 2))
             screen.blit(numbers, numbersRect)
-        x = 50
-        y += 60
 
-    # flip() the display to put your work on screen
-    pygame.display.flip()
+        workingy += 60
 
-    clock.tick(60)  # limits FPS to 60
-
-pygame.quit()
+if __name__ == "__main__":
+    main()
